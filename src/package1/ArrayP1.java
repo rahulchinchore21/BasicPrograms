72306d5b593293ab471a3a1713a05eab70e7f824
rahulchinchore21 <rahul.chinchore@gmail.com>
2020-09-04 22:12:05 +0530

    ArrayProgram

---
+++ src/package1/ArrayP1.java
@@ -0,0 +1,21 @@
+package package1;
+import java.util.Scanner;
+public class ArrayP1 {
+	public static void main(String[] args) {
+		int[][] arr1= new int[3][4];
+		String str= "How are you";
+		Scanner sc=new Scanner(System.in);
+		/*System.out.println("Enter 3 numbers:");
+		for(int i=0;i<a.length;i++) {
+			a[i]=sc.nextInt();
+		}
+		System.out.println("The 3 elements in array are:");
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+			
+		}*/
+		/*System.out.println("Array Size: "+e.length);
+		System.out.println("String Length: "+str.length());
+		System.out.println("Array Second dimension Length: "+d.length);*/
+	}
+}
